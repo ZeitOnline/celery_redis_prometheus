cc9083a50cd841b183eb2bc548878a3ae9619055
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2026-07-27 09:08:54 +0200

    Back to development: 1.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='celery_redis_prometheus',
-    version='1.5.2',
+    version='1.6.0.dev0',
     author='Die ZEIT Online Engineering',
     author_email='zon-backend@zeit.de',
     url='https://github.com/zeitonline/celery_redis_prometheus',
